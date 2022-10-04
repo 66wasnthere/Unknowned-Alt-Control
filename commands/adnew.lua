a960654f12d12944d179400a408cbbf3624c9eb2
66wasnthere <90938274+66wasnthere@users.noreply.github.com>
2022-10-03 22:21:32 -0500

    Delete commands directory

--- commands/adnew.lua
+++
@@ -1,7 +0,0 @@
-if ADNEW_LOADED == true then return end
-getgenv().ADNEW_LOADED = true
-
-function AdnewCmd(_string_)
-    Settings['admsg'] = _string_
-    Say("Setted new ad message.")
-end
--- commands/unlock.lua
+++
@@ -1,9 +0,0 @@
-if UNLOCK_LOADED == true then return end
-getgenv().UNLOCK_LOADED = true
-function UnlockCmd()
-    local plr = game:service"Players".LocalPlayer
-    if workspace:FindFirstChild("AIRLOCK_PART") then
-        workspace:FindFirstChild("AIRLOCK_PART"):Destroy()
-    end
-    plr.Character.HumanoidRootPart.Anchored = false
-end
